6ea96528a1d0ae314b8e118ad967ab8b2e2e7d0c
Monster-Mohamed <ma8494927@gmail.com>
2022-02-02 07:47:09 +0200

    Quiz-English v3
    Added Progress and handling home page and secure the app

--- src/ts/components/AddQuestions.ts
+++ src/ts/components/AddQuestions.ts
@@ -7,19 +7,28 @@ const AddQuestions = (question: Question) => {
         // heading area
         const quizArea = <HTMLElement>document.querySelector(".quiz-area");
         quizArea.innerHTML = `<h2>${question.title}</h2>`;
+        // handling answers
+        let answers = [];
+        answers.push({"a_1": question.a_1});
+        answers.push({"a_2": question.a_2});
+        answers.push({"a_3": question.a_3});
+        answers.push({"a_4": question.a_4});
+        answers.sort(() => Math.random() - 0.5);
 
         // the answers
         const quizAnswers = <HTMLElement>document.querySelector(".custom_radio");
-        for (let i = 1; i < 5; i++) {
-            // @ts-ignore
-            let dataAnswer = question[`a_${i}`];
-            let checked = i == 1 && "checked";
-            quizAnswers.innerHTML +=
-                `
-                <input id="a_${i}" data-answer="${dataAnswer}" ${checked} name="answer" type="radio">
-                <label for="a_${i}">${UpperFirst(dataAnswer)}</label>
+        for (let i = 0; i < answers.length; i++) {
+            for (let key in answers[i]) {
+                // @ts-ignore
+                let value = answers[i][key];
+                let checked = i == 0 && "checked";
+                quizAnswers.innerHTML +=
+                    `
+                <input id="${key}" data-answer="${value}" ${checked} name="answer" type="radio">
+                <label for="${key}"> ${UpperFirst(value)} </label>
                 <br>
             `;
+            }
         }
     }
 };
